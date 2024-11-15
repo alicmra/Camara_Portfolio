3ef5b88e9a548c6ac9a7718978242d28840e46d1
Alliana Marie Camara <allianamarie.camara@tup.edu.ph>
2024-11-15 16:36:12 +0800

    added skills

--- script.js
+++ script.js
@@ -5,3 +5,22 @@ hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
 });
+
+let sections =document.querySelectorAll('sections');
+let navLinks =document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec =>{
+        let top =window.screenY;
+        let offset = sec.offsetTop;
+        let heighht = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height){
+            navLinks.forEach(links =>{
+                links.classList.remove('activate');
+                document.querySelector('header nav a [href*= '+id +')').classList.add('activate');
+            });
+        }
+    });
+};
